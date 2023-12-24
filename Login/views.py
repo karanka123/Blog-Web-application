a95f67e023f4ad3cc9408aa6afd911ab0453f6b8
Karan-KA <142216725+Karan-KA@users.noreply.github.com>
2023-12-24 10:36:02 +0000

    registeration

--- Login/views.py
+++ Login/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.urls import reverse
-# Create your views here.
+
 
 def login_view(request):
     if request.method == 'POST':

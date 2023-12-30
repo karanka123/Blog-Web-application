88d0b7c84140b52bf2371cfd38752e21b7c09820
karanka123 <154742613+karanka123@users.noreply.github.com>
2023-12-30 06:04:57 +0000

    Many to Many interface

--- Login/models.py
+++ Login/models.py
@@ -5,9 +5,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class User(models.Model):
+    username = models.CharField(max_length=24)
+    blogs = models.ManyToManyField('Blog', related_name='user_blogs')
+
+
 class blog(models.Model):
     Author = models.CharField(max_length = 24)
     Title = models.CharField(max_length = 255)
     Content= models.CharField(max_length = 500)
     Image = models.ImageField(null=True, blank=True)
-    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'user')
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'post_set')
+
+    def __str__(self):
+        return self.Author
+    
